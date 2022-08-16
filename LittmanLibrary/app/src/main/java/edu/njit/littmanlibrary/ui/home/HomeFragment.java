6ca9b6d78430de04e2a4de4151fec29a38ccaec5
Ayon Chowdhury <md.rajib.sarwar@gmail.com>
2022-08-16 02:52:33 -0400

    created horizontal scrollview with sample items and created a base adapter to populate the list view

--- LittmanLibrary/app/src/main/java/edu/njit/littmanlibrary/ui/home/HomeFragment.java
+++ LittmanLibrary/app/src/main/java/edu/njit/littmanlibrary/ui/home/HomeFragment.java
@@ -1,37 +1,90 @@
 package edu.njit.littmanlibrary.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
-import edu.njit.littmanlibrary.databinding.FragmentHomeBinding;
+import edu.njit.littmanlibrary.R;
+import edu.njit.littmanlibrary.helpers.HorizontalScrollListView;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        HorizontalScrollListView listview1 = (HorizontalScrollListView) root.findViewById(R.id.listview1);
+        HomeAdapter mAdapter = new HomeAdapter(getActivity(), publications);
+        listview1.setAdapter(mAdapter);
+        HorizontalScrollListView listview2 = (HorizontalScrollListView) root.findViewById(R.id.listview2);
+        mAdapter = new HomeAdapter(getActivity(), events);
+        listview2.setAdapter(mAdapter);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private static Entry[] publications = new Entry[]{
+            new Entry("one", "Text #1"),
+            new Entry("two", "Text #2"),
+            new Entry("three", "Text #3"),
+            new Entry("four", "Text #4")};
+
+    private static Entry[] events = new Entry[]{
+            new Entry("five", "Music in the Library Event"),
+            new Entry("six", "Book Talk with Rima Taher"),
+            new Entry("seven", "The World of Robotics"),
+            new Entry("eight", "INTERNATIONAL TEA WITH ALUMNI")};
+
+    public class HomeAdapter extends BaseAdapter {
+        Entry[] dataObjects;
+
+        public HomeAdapter(Context context, Entry[] dataObjects) {
+            this.dataObjects = dataObjects;
+        }
+
+        @Override
+        public int getCount() {
+            return dataObjects.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View retval = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_item, null);
+            TextView title = (TextView) retval.findViewById(R.id.title);
+            ImageView image = (ImageView) retval.findViewById(R.id.image);
+            int resourceId = getResources().getIdentifier(dataObjects[position].image, "drawable", getActivity().getPackageName());
+            image.setImageResource(resourceId);
+            title.setText(dataObjects[position].title);
+
+            return retval;
+        }
+
+    };
+
+    private static class Entry {
+        String image = "";
+        String title = "";
+
+        public Entry(String image, String title) {
+            this.image = image;
+            this.title = title;
+        }
     }
 }
